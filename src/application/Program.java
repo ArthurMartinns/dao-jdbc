081bd6d6d86a74efbf11122f453b12f5e3e1c145
ArthurMartinns <arthurmartiinscontato@gmail.com>
2026-01-09 17:20:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "First Department");
-		System.out.print(obj);
+		Seller seller = new Seller(21, "Arthur", "arthur@gmail.com", new Date(0), 5000.0, obj);
+	
+		System.out.print(seller);
 	}
 
 }
