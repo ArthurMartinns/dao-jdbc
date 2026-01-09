094b534e65fe4a7c49e9c30f9779a65837edb368
ArthurMartinns <arthurmartiinscontato@gmail.com>
2026-01-09 19:51:56 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,25 @@
 package application;
 
-import java.sql.Date;
+import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "First Department");
-		Seller seller = new Seller(21, "Arthur", "arthur@gmail.com", new Date(0), 5000.0, obj);
-	
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		System.out.print(seller);
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
