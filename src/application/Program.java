c2451896aa06c4ed8c3cca2178ddff63e55e72c8
ArthurMartinns <arthurmartiinscontato@gmail.com>
2026-01-10 10:01:36 -0300

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
